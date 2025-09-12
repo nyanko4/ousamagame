c3671d0ea7cfbb8f0f471ea4e544d1977cb66136
nyanko4 <nyankobetsuaka@outlook.com>
2025-09-12 12:08:52 +0900

    Create gamesystem.js

---
+++ ctr/gamesystem.js
@@ -0,0 +1,21 @@
+const _ = require("lodash");
+
+function getShuffleFunction(method) {
+  switch (method) {
+    case "shuffle":
+      return shuffle;
+    default:
+      return _.shuffle;
+  }
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+module.exports = {
+  getShuffleFunction,
+};
