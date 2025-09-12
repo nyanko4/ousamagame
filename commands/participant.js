f0e0d53d95c84130f3332fb344fc676368714c9d
nyanko4 <nyankobetsuaka@outlook.com>
2025-09-12 14:06:47 +0900

    Update participant.js

--- commands/participant.js
+++ commands/participant.js
@@ -1,6 +1,6 @@
 const _ = require("lodash");
 const { readFileAsync, writeFileAsync } = require("../lib/supabase_file");
-const { sendchatwork } = require("../ctr/message");
+const { sendchatwork, replyMessage } = require("../ctr/message");
 const adminAccountId = process.env.adminAccountId;
 
 // 入力決定
@@ -33,15 +33,9 @@ async function participantAdd(body, messageId, roomId, accountId) {
   const Id = body.replace("追加", "");
   let participant = await readFileAsync("participant");
   if (participant.includes(Id)) {
-    await sendchatwork(
-      `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n既に追加されています`,
-      roomId
-    );
+    await replyMessage(accountId, roomId, messageId, "既に追加されています");
   } else {
-    await sendchatwork(
-      `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n[pname:${Id}]さんを追加します`,
-      roomId
-    );
+    await replyMessage(accountId, roomId, messageId, `[pname:${Id}]さんを追加します`);
     participant.push(Id);
     participant = _.uniq(participant).filter(p => p !== "" && p !== ",");
     await writeFileAsync("participant", participant);
@@ -53,44 +47,29 @@ async function participantDelete(body, messageId, roomId, accountId) {
   const account = body.replace("削除", "");
   let participant = await readFileAsync("participant");
   if (participant.includes(account)) {
-    await sendchatwork(
-      `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n[pname:${account}]さんを削除します`,
-      roomId
-    );
+    await replyMessage(accountId, roomId, messageId, `[pname:${account}]さんを削除します`);
     participant = _.uniq(participant).filter(
       p => p !== account && p !== "," && p !== ""
     );
     await writeFileAsync("participant", participant);
   } else {
-    await sendchatwork(
-      `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n既に削除されています`,
-      roomId
-    );
+    await replyMessage(accountId, roomId, messageId, `既に削除されています`);
   }
 }
 
 // clear
 async function participantClear(messageId, roomId, accountId) {
   await writeFileAsync("participant", []);
-  await sendchatwork(
-    `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n全て削除しました`,
-    roomId
-  );
+  await replyMessage(accountId, roomId, messageId, "全て削除しました");
 }
 
 // 参加申請
 async function participation(messageId, roomId, accountId) {
   let participant = await readFileAsync("participant");
   if (participant.includes(accountId)) {
-    await sendchatwork(
-      `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\nもう参加が受理されています`,
-      roomId
-    );
+    await replyMessage(accountId, roomId, messageId, "もう参加が受理されています");
   } else {
-    await sendchatwork(
-      `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n参加を受理します`,
-      roomId
-    );
+    await replyMessage(accountId, roomId, messageId, "参加を受理します");
     participant.push(accountId);
     participant = _.uniq(participant).filter(p => p !== "" && p !== ",");
     await writeFileAsync("participant", participant);
@@ -103,23 +82,14 @@ async function exit(messageId, roomId, accountId) {
   const canExit = await readFileAsync("canExit");
   if (canExit === "可" || accountId === adminAccountId) {
     if (participant.includes(accountId)) {
-      await sendchatwork(
-        `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n退出を受理します`,
-        roomId
-      );
+      await replyMessage(accountId, roomId, messageId, "退出を受理します");
     } else {
-      await sendchatwork(
-        `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\nもう退出されています`,
-        roomId
-      );
+      await replyMessage(accountId, roomId, messageId, "もう退出されています");
     }
     participant = _.uniq(participant).filter(p => p != accountId && p !== ",");
     await writeFileAsync("participant", participant);
   } else {
-    await sendchatwork(
-      `[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n使用不可です`,
-      roomId
-    );
+    await replyMessage(accountId, roomId, messageId, "使用不可です");
   }
 }
 
